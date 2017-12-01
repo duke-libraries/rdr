8f3f931feae770f067c5fec8831ef92277924c6e
Jim Coble <jim.coble@duke.edu>
2017-12-01 15:27:13 -0500

    Call ActiveFedora::Cleaner.clean! at beginning of test suite; RDR-88
    Otherwise, test leaves default admin set in Fedora repo but not in Solr or permission_templates table.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'active_fedora/cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -54,4 +55,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:suite) do
+    ActiveFedora::Cleaner.clean!
+  end
+
 end
