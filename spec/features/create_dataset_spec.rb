3f94998e1dffe8b8782702b2c593bea4a5ae54ac
Jim Coble <jim.coble@duke.edu>
2017-12-13 14:19:32 -0500

    Reactivate commented out spec feature test, adapting to new user model. (#15)

--- spec/features/create_dataset_spec.rb
+++ spec/features/create_dataset_spec.rb
@@ -5,29 +5,29 @@ include Warden::Test::Helpers
 
 # NOTE: If you generated more than one work, you have to set "js: true"
 RSpec.feature 'Create a Dataset', js: false do
-  # context 'a logged in user' do
-  #   let(:user_attributes) do
-  #     { email: 'test@example.com' }
-  #   end
-  #   let(:user) do
-  #     User.new(user_attributes) { |u| u.save(validate: false) }
-  #   end
-  #
-  #   before do
-  #     AdminSet.find_or_create_default_admin_set_id
-  #     login_as user
-  #   end
-  #
-  #   scenario do
-  #     visit '/dashboard'
-  #     click_link "Works"
-  #     click_link "Add new work"
-  #
-  #     # If you generate more than one work uncomment these lines
-  #     choose "payload_concern", option: "Dataset"
-  #     click_button "Create work"
-  #
-  #     expect(page).to have_content "Add New Dataset"
-  #   end
-  # end
+  context 'a logged in user' do
+    let(:user_attributes) do
+      { email: 'test@example.com', uid: 'test@example.com' }
+    end
+    let(:user) do
+      User.new(user_attributes) { |u| u.save(validate: false) }
+    end
+
+    before do
+      AdminSet.find_or_create_default_admin_set_id
+      login_as user
+    end
+
+    scenario do
+      visit '/dashboard'
+      click_link "Works"
+      click_link "Add new work"
+
+      # If you generate more than one work uncomment these lines
+      # choose "payload_concern", option: "Dataset"
+      # click_button "Create work"
+
+      expect(page).to have_content "Add New Dataset"
+    end
+  end
 end
