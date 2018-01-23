7e27d9ec79bdff85892f408f50c6922c06fa153c
David Chandek-Stark <dchandekstark@gmail.com>
2018-01-23 14:51:39 -0500

    Completes RDR-141.
    Customized provenance and bibliographic citation metadata form partials.

--- app/forms/hyrax/dataset_form.rb
+++ app/forms/hyrax/dataset_form.rb
@@ -2,7 +2,31 @@
 #  `rails generate hyrax:work Dataset`
 module Hyrax
   class DatasetForm < Hyrax::Forms::WorkForm
+
     self.model_class = ::Dataset
-    self.terms += [:resource_type]
+
+    self.terms += [
+      :resource_type,
+      :affiliation,
+      :alternative,
+      :ark,
+      :available,
+      :doi,
+      :format,
+      :provenance,
+      :temporal,
+    ]
+
+    self.required_fields = [
+      :title,
+      :creator,
+      :publisher,
+      :available,
+      :description,
+      :resource_type,
+      :license,
+      :bibliographic_citation,
+    ]
+
   end
 end
