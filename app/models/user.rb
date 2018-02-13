5ef4d555c6619412d63615a99903c2385c0455ee
ad96 <ad96@duke.edu>
2018-02-13 13:05:00 -0500

    Adds all uids in group 'curator' in role_map.yml as proxy depositors … (#55)
    * Adds all uids in group 'curator' in role_map.yml as proxy depositors for a new user. (RDR-165).
    
    * part two.. changed User instance method to class method, changed iterator name to curator_key, changed
    group_services.map to Rolemapper.whois(), which returns nil array when needed.
    
    * part 3.. updated the spec test to match recent change of RoleMapper.whois() in curators().

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   include Hyrax::User
   include Hyrax::UserUsageStats
 
+  CURATOR_GROUP = 'curator'
+
   if Blacklight::Utils.needs_attr_accessible?
     attr_accessible :email, :password, :password_confirmation
   end
@@ -23,23 +25,32 @@ class User < ApplicationRecord
 
   validates :uid, presence: true, uniqueness: true
 
-  # @param auth [OmniAuth::AuthHash] authenticated user information.
-  # @return [User] the authenticated user, possibly a newly created record.
-  # @see {User::OmniauthCallbacksController#shibboleth}
-  def self.from_omniauth(auth)
-    find_or_initialize_by(uid: auth.uid).tap do |user|
-      user.password = Devise.friendly_token if user.new_record?
-      user.update!(email: auth.info.email, display_name: auth.info.name)
-    end
-  end
+  after_create :add_curators_as_proxies
 
-  # Override method from Hyrax::User::ClassMethods so that it properly creates system users,
-  # given that we do not use 'email' for Hydra.config.user_key_field.
-  def self.find_or_create_system_user(user_key)
-    User.find_by_user_key(user_key) ||
-        User.create!(Hydra.config.user_key_field => user_key,
-                     email: user_key,
-                     password: Devise.friendly_token[0, 20])
+  class << self
+    # @param auth [OmniAuth::AuthHash] authenticated user information.
+    # @return [User] the authenticated user, possibly a newly created record.
+    # @see {User::OmniauthCallbacksController#shibboleth}
+    def from_omniauth(auth)
+      find_or_initialize_by(uid: auth.uid).tap do |user|
+        user.password = Devise.friendly_token if user.new_record?
+        user.update!(email: auth.info.email, display_name: auth.info.name)
+      end
+    end
+
+    # Override method from Hyrax::User::ClassMethods so that it properly creates system users,
+    # given that we do not use 'email' for Hydra.config.user_key_field.
+    def find_or_create_system_user(user_key)
+      User.find_by_user_key(user_key) ||
+          User.create!(Hydra.config.user_key_field => user_key,
+                       email: user_key,
+                       password: Devise.friendly_token[0, 20])
+    end
+
+    def curators
+      RoleMapper.whois(CURATOR_GROUP)
+    end
+
   end
 
   # Method added by Blacklight; Blacklight uses #to_s on your
@@ -52,4 +63,14 @@ class User < ApplicationRecord
   def user_key
     uid
   end
+
+  def add_curators_as_proxies
+    unless [ User.audit_user_key, User.batch_user_key ].include?(self.user_key)
+      curators = self.class.curators
+      curators.each do |curator_key|
+        ProxyService.add(user: self, proxy: curator_key)
+      end
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,24 @@ RSpec.describe User do
     end
   end
 
+ describe "#add_curators_as_proxies" do
+
+    let(:curatorA) { FactoryBot.create(:user) }
+    let(:curatorB) { FactoryBot.create(:user) }
+
+    before do
+      # stub group service implementation to return curator group members
+      allow(RoleMapper).to receive(:whois).with(User::CURATOR_GROUP).and_return([ curatorA.uid, curatorB.uid ])
+    end
+
+    it "adds all curators as proxy depositors for new user" do
+      owner_user = FactoryBot.create(:user, email: "user@example.com")
+      expect(owner_user.can_receive_deposits_from).to match_array([curatorA, curatorB])
+    end
+
+    it "does not add curators as proxy depositors for new system users" do
+      expect(User.audit_user.can_receive_deposits_from).to be_empty
+      expect(User.batch_user.can_receive_deposits_from).to be_empty
+     end
+  end
 end
