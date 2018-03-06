a506be36a172b70beeff8a244023641863566054
Jim Coble <jim.coble@duke.edu>
2018-03-06 16:57:47 -0500

    Notify importer on checksum verification failure; finishes RDR-174. (#67)

---
+++ spec/mailers/checksum_verification_mailer_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe ChecksumVerificationMailer do
+
+  let(:user) { FactoryBot.create(:user) }
+  let(:error_msg) { 'invalid checksum error message' }
+  let(:error) { Rdr::ChecksumInvalid.new(error_msg) }
+
+  describe "notify_failure" do
+    it "works" do
+      described_class.notify_failure(error, user).deliver_now!
+      mail = ActionMailer::Base.deliveries.last
+      expect(mail.subject).to eq(I18n.t('rdr.checksum_verification_failure_heading'))
+      expect(mail.body).to match(error_msg)
+    end
+  end
+
+end
