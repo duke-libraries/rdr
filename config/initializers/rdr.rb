1c45abf9e3bb9075c85c7caae300b65ab480afde
Jim Coble <jim.coble@duke.edu>
2018-04-06 15:22:36 -0400

    Use EZID test mode for all non-production environments; RDR-198. (#93)

--- config/initializers/rdr.rb
+++ config/initializers/rdr.rb
@@ -3,3 +3,9 @@ require 'rdr'
 Rdr.configure do |config|
   config.preferred_admin_set_id = ENV['PREFERRED_ADMIN_SET_ID'] || AdminSet::DEFAULT_ID
 end
+
+# use EZID test mode for non-production environments
+unless Rails.env.production?
+  require 'ezid/test_helper'
+  ezid_test_mode!
+end
