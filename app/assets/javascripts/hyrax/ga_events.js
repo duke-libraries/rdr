cb066f6bce4f3cd0844b5847df6a66c3562a9340
Sean Aery <sean.aery@duke.edu>
2018-04-25 15:34:49 -0400

    Updated Google Analytics event data syntax to use gtag.js for tracking file downloads. Fixes RDR-213. (#115)

--- app/assets/javascripts/hyrax/ga_events.js
+++ app/assets/javascripts/hyrax/ga_events.js
@@ -1,6 +1,11 @@
 // Overrides hyrax/app/assets/javascripts/hyrax/ga_events.js
 // in order to use gtag.js library instead of legacy ga.js
 
+// Documentation: https://developers.google.com/analytics/devguides/collection/gtagjs/events
+
 $(document).on('click', '#file_download', function(e) {
-  gtag('event', 'Files', 'Downloaded', $(this).data('label'));
+  gtag('event', 'Downloaded', {
+    'event_category': 'Files',
+    'event_label': $(this).data('label')
+  });
 });
