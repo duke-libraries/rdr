87e49e221c5894eb01751d56da2c21d671c8bbc3
Michael Daul <michael.daul@duke.edu>
2018-05-17 09:43:20 -0400

    turned off gallery, masonry, and slideshow views -- commented out configurations (#136)

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -14,9 +14,9 @@ class CatalogController < ApplicationController
   end
 
   configure_blacklight do |config|
-    config.view.gallery.partials = [:index_header, :index]
-    config.view.masonry.partials = [:index]
-    config.view.slideshow.partials = [:index]
+    #config.view.gallery.partials = [:index_header, :index]
+    #config.view.masonry.partials = [:index]
+    #config.view.slideshow.partials = [:index]
 
 
     config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
@@ -24,8 +24,8 @@ class CatalogController < ApplicationController
     config.search_builder_class = SearchBuilder
 
     # Show gallery view
-    config.view.gallery.partials = [:index_header, :index]
-    config.view.slideshow.partials = [:index]
+    #config.view.gallery.partials = [:index_header, :index]
+    #config.view.slideshow.partials = [:index]
 
     ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
     config.default_solr_params = {
