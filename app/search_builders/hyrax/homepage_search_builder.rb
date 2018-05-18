bcb3f1fd56975ec2198723190cc393af1b8d57aa
Jim Coble <jim.coble@duke.edu>
2018-05-18 12:31:10 -0400

    Add comments about overrides of Hyrax / Hydra code. (#139)

--- app/search_builders/hyrax/homepage_search_builder.rb
+++ app/search_builders/hyrax/homepage_search_builder.rb
@@ -1,3 +1,4 @@
+# Override Hyrax::HomepageSearchBuilder to add filter for only top-level works
 class Hyrax::HomepageSearchBuilder < Hyrax::SearchBuilder
   include Hyrax::FilterByType
   self.default_processor_chain += [ :add_access_controls_to_solr_params, :only_top_level_works_filter ]
--- lib/hydra/works/models/concerns/file_set/mime_types.rb
+++ lib/hydra/works/models/concerns/file_set/mime_types.rb
@@ -1,4 +1,4 @@
-# This was taken directly from Sufia's GenericFile::MimeTypes
+# Overrides Hydra::Works::MimeTypes
 module Hydra::Works
   module MimeTypes
     extend ActiveSupport::Concern
