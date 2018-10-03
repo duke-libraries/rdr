09793696c3d6248d2f6c81998487bbb9ef894afb
Jim Coble <jim.coble@duke.edu>
2018-10-03 16:42:35 -0400

    Undefine VISIBILITY_LABEL_CLASS constant before redefining it to eliminate already initialized constant warning; RDR-391. (#246)

--- app/prepends/prepended_presenters/permission_badge.rb
+++ app/prepends/prepended_presenters/permission_badge.rb
@@ -3,12 +3,16 @@ module PrependedPresenters::PermissionBadge
   # Uses Rails to_prepare hook in config/application.rb
   # See http://samvera.github.io/patterns-presenters.html#overriding-and-custom-presenter-methods
 
+  if Hyrax::PermissionBadge.const_defined?(:VISIBILITY_LABEL_CLASS)
+    Hyrax::PermissionBadge.send(:remove_const, :VISIBILITY_LABEL_CLASS)
+  end
+
   Hyrax::PermissionBadge.const_set('VISIBILITY_LABEL_CLASS',{
     authenticated: "label-info permission-badge-authenticated",
     embargo: "label-warning permission-badge-embargo",
     lease: "label-warning permission-badge-lease",
     open: "label-success permission-badge-open",
-    restricted: "label-danger permission-badge-restricted"    
+    restricted: "label-danger permission-badge-restricted"
   })
 
 end
