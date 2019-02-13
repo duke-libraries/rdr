8af2d3da33715f5fac75a61364b2c92fcdb19c4d
Jim Coble <jim.coble@duke.edu>
2019-02-13 14:58:57 -0500

    Clean-up refactoring for virus checking. (#256)

--- app/services/rdr/virus_scanner.rb
+++ app/services/rdr/virus_scanner.rb
@@ -6,18 +6,15 @@ module Rdr
     def infected?
       begin
         Ddr::Antivirus.scan(file)
+      rescue Ddr::Antivirus::ScannerError => e
+        Rails.logger&.error(e)
+        raise e
       rescue Ddr::Antivirus::VirusFoundError => e
-        warning(I18n.t('rdr.virus_found', file: file, error_msg: e.message))
+        Rails.logger&.warn(I18n.t('rdr.virus_found', file: file, error_msg: e.message))
         true
       else
         false
       end
-    end
-
-    private
-
-    def warning(msg)
-      Rails.logger&.warn(msg)
     end
 
   end
