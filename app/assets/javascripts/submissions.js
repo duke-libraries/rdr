42711ad36161e13a8a8dae0d3abf304764a571eb
Michael Daul <michael.daul@duke.edu>
2019-11-18 16:24:19 -0500

    reworked the submission JS to catch some bugs (#303)
    * reworked the submission JS to catch some bugs
    - disables 'next' button on required fields
    - textarea questions are checked on change
    - for skip logic question, resets child vaules for branch a when parent b is retroactively selected
    
    * forgot to cleanup console logging

--- app/assets/javascripts/submissions.js
+++ app/assets/javascripts/submissions.js
@@ -5,9 +5,10 @@ var currentTab = 0; // First tab is 0
 var totalTabs = 7;
 showTab(currentTab); // Display the current tab
 
+
 function showTab(n) {
-  var x = document.getElementsByClassName("form-tab");
-  x[n].style.display = "block";
+  var theFormTab = document.getElementsByClassName("form-tab");
+  theFormTab[n].style.display = "block";
 
   if (n == 0) {
     document.getElementById("prevBtn").style.display = "none";
@@ -20,10 +21,61 @@ function showTab(n) {
     document.getElementById("nextBtn").innerHTML =
       "<span class='sr-only'>Submit</span><span aria-hidden='true'>Submit &gt;&gt;</span>";
   }
+
+  if (currentTab == 2) {
+
+    // reset data sizes if needed
+    if ( $("input[name='submission[screening_funding]']:checked").val() == "yes" ) {
+      $('#nonfunded-size-1').parent().removeClass( "active" );
+      $('#nonfunded-size-2').parent().removeClass( "active" );
+      document.getElementById("nonfunded-size-1").checked = false;
+      document.getElementById("nonfunded-size-2").checked = false;
+    }
+    if ( $("input[name='submission[screening_funding]']:checked").val() == "no" ) {
+      $('#funded-size-1').parent().removeClass( "active" );
+      $('#funded-size-2').parent().removeClass( "active" );
+      document.getElementById("funded-size-1").checked = false;
+      document.getElementById("funded-size-2").checked = false;
+    }
+  }
+
+  if (currentTab == 5) {
+    // set error display if needed
+    if ($("#t5 .text-area-empty").length > 0) {
+      addError();
+    } else {
+      removeError();
+    }
+  }
+
+  if (currentTab == 6) {
+    // set error display if needed
+    if ( $("input[name='submission[doi_exists]']:checked").val() == "yes" && $("#submission_doi").val() == '' ) {
+      addError();
+      document.getElementById("submission_doi").classList.add("text-area-empty");
+    } else {
+      removeError();
+      document.getElementById("submission_doi").classList.remove("text-area-empty");
+    }
+  }
+
+  if (currentTab == 7) {
+    // set error display if needed
+    if ( $("input[name='submission[using_cc0]']:checked").val() == "will not use cc0" && $("#t7 select").val() == '' ) {
+      addError();
+      document.getElementById("submission_license").classList.add("text-area-empty");
+    } else {
+      removeError();
+      document.getElementById("submission_license").classList.remove("text-area-empty");
+    }
+  }
+
 }
 
+
+
 function nextPrev(n) {
-  var x = document.getElementsByClassName("form-tab");
+  var theFormTab = document.getElementsByClassName("form-tab");
 
   if (n == 1 && !validateForm()) {
     return false;
@@ -47,15 +99,12 @@ function nextPrev(n) {
     return false;
   }
 
-  x[currentTab].style.display = "none";
+  theFormTab[currentTab].style.display = "none";
 
   // if previous button
   if (n == -1) {
     submitForm = false;
-    document
-      .getElementById("t" + currentTab)
-      .classList.remove("form-tab-error");
-    document.getElementById("form_alert" + currentTab).classList.add("hidden");
+    removeError();
     document.getElementById("nextBtn").innerHTML =
       "<span class='sr-only'>Next</span><span aria-hidden='true'>&gt;&gt;</span>";
     document.getElementById("t" + currentTab).focus(); // focus on content
@@ -63,54 +112,62 @@ function nextPrev(n) {
 
   currentTab = currentTab + n;
 
-  //console.log('current tab: ' + currentTab);
+  // console.log('current tab: ' + currentTab);
 
   showTab(currentTab);
 }
 
+
 // string values passed from rails constants
-var t4a_exclude = document.getElementById("submission-values").getAttribute("data-t4-more-than-2-5");
-var t4b_exclude = document.getElementById("submission-values").getAttribute("data-t4-more-than-10");
-var t5_exclude = document.getElementById("submission-values").getAttribute("data-t5-not-agree");
-var t8_exclude = document.getElementById("submission-values").getAttribute("data-t8-not-use");
+var t3a_exclude = document.getElementById("submission-values").getAttribute("data-t3-more-than-2-5");
+var t3b_exclude = document.getElementById("submission-values").getAttribute("data-t3-more-than-10");
+var t4_exclude = document.getElementById("submission-values").getAttribute("data-t4-not-agree");
+var t7_exclude = document.getElementById("submission-values").getAttribute("data-t7-not-use");
+
+
 
 function validateForm() {
-  var x,
-    y,
+  var myFormTab,
+    myInput,
+    myTextArea,
     i,
     j,
     valid = true;
 
-  x = document.getElementsByClassName("form-tab");
-  y = x[currentTab].getElementsByTagName("input");
-  z = x[currentTab].getElementsByTagName("textarea");
+  myFormTab = document.getElementsByClassName("form-tab");
+  myInput = myFormTab[currentTab].getElementsByTagName("input");
+  myTextArea = myFormTab[currentTab].getElementsByTagName("textarea");
 
   // Check every input field in the current tab:
-  for (i = 0; i < y.length; i++) {
+  for (i = 0; i < myInput.length; i++) {
     // If it's a radio button
-    if (y[i].type == "radio") {
-      if (y[i].checked == true) {
+    if (myInput[i].type == "radio") {
+      if (myInput[i].checked == true) {
         // Submit early for certain responses
 
-        if (currentTab == 1 && y[i].value == "yes") {
+        if (currentTab == 0) {
+          document.getElementById("nextBtn").classList.remove("disabled");
+        }
+
+        if (currentTab == 1 && myInput[i].value == "yes") {
           submitForm = true;
         }
 
-        if (currentTab == 3 && y[i].value == t4b_exclude) {
+        if (currentTab == 3 && myInput[i].value == t3b_exclude) {
           submitForm = true;
         }
 
-        if (currentTab == 3 && y[i].value == t4a_exclude) {
+        if (currentTab == 3 && myInput[i].value == t3a_exclude) {
           submitForm = true;
         }
 
-        if (currentTab == 4 && y[i].value == t5_exclude) {
+        if (currentTab == 4 && myInput[i].value == t4_exclude) {
           submitForm = true;
         }
 
         // Implement skip logic
 
-        if (currentTab == 2 && y[i].value == "no") {
+        if (currentTab == 2 && myInput[i].value == "no") {
           document.getElementById("screening-3a").classList.add("hidden");
           document.getElementById("screening-3b").classList.remove("hidden");
           document.getElementById("funded-size-1").disabled = true;
@@ -119,7 +176,7 @@ function validateForm() {
           document.getElementById("nonfunded-size-2").disabled = false;
         }
 
-        if (currentTab == 2 && y[i].value == "yes") {
+        if (currentTab == 2 && myInput[i].value == "yes") {
           document.getElementById("screening-3a").classList.remove("hidden");
           document.getElementById("screening-3b").classList.add("hidden");
           document.getElementById("funded-size-1").disabled = false;
@@ -128,19 +185,31 @@ function validateForm() {
           document.getElementById("nonfunded-size-2").disabled = true;
         }
 
+        // Manually check funding size (byproduct of skip logic)
+        if ( currentTab == 3 && $("input[name='submission[screening_funding]']:checked").val() == "yes" ) {
+          checkingVal = $("input[name='submission[screening_funded_size]']:checked").val();
+          if ( !checkingVal ) {
+            addError();
+            valid = false;
+            break;
+          }
+        }
+
+        if ( currentTab == 3 && $("input[name='submission[screening_funding]']:checked").val() == "no" ) {
+          checkingVal = $("input[name='submission[screening_nonfunded_size]']:checked").val();
+          if ( !checkingVal ) {
+            addError();
+            valid = false;
+            break;
+          }
+        }
+
         // Manually check text field inputs
 
-        if (currentTab == 6 && y[i].value == "yes") {
+        if (currentTab == 6 && myInput[i].value == "yes") {
           if (document.getElementById("submission_doi").value == "") {
-            document
-              .getElementById("submission_doi")
-              .classList.add("text-area-empty");
-            document
-              .getElementById("t" + currentTab)
-              .classList.add("form-tab-error");
-            document
-              .getElementById("form_alert" + currentTab)
-              .classList.remove("hidden");
+            document.getElementById("submission_doi").classList.add("text-area-empty");
+            addError();
             valid = false;
             break;
           }
@@ -152,84 +221,127 @@ function validateForm() {
           }
         }
 
-        if (currentTab == 7 && y[i].value == t8_exclude) {
+        if (currentTab == 7 && myInput[i].value == t7_exclude) {
           if (document.getElementById("submission_license").value == "") {
-            document
-              .getElementById("submission_license")
-              .classList.add("text-area-empty");
-            document
-              .getElementById("t" + currentTab)
-              .classList.add("form-tab-error");
-            document
-              .getElementById("form_alert" + currentTab)
-              .classList.remove("hidden");
+            addError();
             valid = false;
             break;
           }
 
           if (document.getElementById("submission_license").value != "") {
-            document
-              .getElementById("submission_license")
-              .classList.remove("text-area-empty");
+            document.getElementById("submission_license").classList.remove("text-area-empty");
           }
         }
 
         valid = true;
-
-        document
-          .getElementById("t" + currentTab)
-          .classList.remove("form-tab-error");
-        document
-          .getElementById("form_alert" + currentTab)
-          .classList.add("hidden");
+        removeError();
         document.getElementById("t" + currentTab).focus();
 
         break;
-      } else if (y[i].checked == false) {
+
+      } else if (myInput[i].checked == false) {
         valid = false;
-        document
-          .getElementById("t" + currentTab)
-          .classList.add("form-tab-error");
-        document
-          .getElementById("form_alert" + currentTab)
-          .classList.remove("hidden");
+        addError();
         window.location.hash = "#form_alert" + currentTab;
       }
     }
   }
 
   // Loop through text areas
-  for (j = 0; j < z.length; j++) {
-    if (z[j].required) {
-      if (z[j].value == "") {
+  for (j = 0; j < myTextArea.length; j++) {
+    if (myTextArea[j].required) {
+      if (myTextArea[j].value == "") {
         valid = false;
-        z[j].classList.add("text-area-empty");
-        document
-          .getElementById("t" + currentTab)
-          .classList.add("form-tab-error");
-        document
-          .getElementById("form_alert" + currentTab)
-          .classList.remove("hidden");
+        myTextArea[j].classList.add("text-area-empty");
+        addError();
       }
 
-      if (z[j].value != "") {
-        z[j].classList.remove("text-area-empty");
+      if (myTextArea[j].value != "") {
+        myTextArea[j].classList.remove("text-area-empty");
       }
     }
   }
 
-
   return valid;
 }
 
-// Show/hide text fields
 
+// +++ clean up error display +++ //
+
+  // undo disabled nextBtn for radio buttons
+  $(function() {
+
+    $('input[type="radio"]').bind('change', function () {
+      document.getElementById("nextBtn").classList.remove("disabled");
+    });
+
+  });
+
+  // undo disabled nextBtn for text areas (tab 5)
+  $( "#t5 textarea" ).on("change", function() {
+      
+    var zeroCount = 0;
+
+    $(".text-area-empty").each(function() {
+      if ( $(this).val().length == 0) {
+        zeroCount++
+      } else {
+        $(this).removeClass( "text-area-empty" );
+      }
+    });
+
+    if (zeroCount == 0) {
+      removeError();
+    }
+  
+  });
+
+  // undo disabled nextBtn for text input (tab 6)
+  $(function() {
+
+    $('#t6 input[type="text"]').bind('change', function () {
+      if ( $(this).val().length != 0) {
+        $(this).removeClass( "text-area-empty" );
+        document.getElementById("nextBtn").classList.remove("disabled");
+      }
+    });
+
+  });
+
+  // undo disabled nextBtn for select (tab 7)
+  $( "#t7 select" ).on("change", function() {
+
+    if ($(this).val() != '') {
+      document.getElementById("nextBtn").classList.remove("disabled");
+      $(this).removeClass( "text-area-empty" );
+    } else {
+      document.getElementById("nextBtn").classList.add("disabled");
+    }
+
+  });
+
+
+// +++ functions +++ //
+
+// Show/hide text fields
 function showMy(theField) {
   document.getElementById(theField + "_wrapper").classList.remove("hidden");
   document.getElementById("submission_" + theField).disabled = false;
 }
-
 function hideMy(theField) {
   document.getElementById(theField + "_wrapper").classList.add("hidden");
   document.getElementById("submission_" + theField).disabled = true;
+}
+
+
+// show/hide error wrappers
+function addError() {
+  document.getElementById("t" + currentTab).classList.add("form-tab-error");
+  document.getElementById("form_alert" + currentTab).classList.remove("hidden");
+  document.getElementById("nextBtn").classList.add("disabled");
+}
+function removeError() {
+  document.getElementById("t" + currentTab).classList.remove("form-tab-error");
+  document.getElementById("form_alert" + currentTab).classList.add("hidden");
+  document.getElementById("nextBtn").classList.remove("disabled");
 }
